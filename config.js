1785c4a3af87330ac29232883946db2e9347f6a1
PoddaModz <139072214+PoddaModz@users.noreply.github.com>
2023-07-10 00:14:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763709174', 'me', true],
+  ['94710410419', 'PODDA_MODZ_OFFLCLAL', true], 
   [''] 
 ] //Number of owners
 
@@ -49,13 +49,13 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = 'PODDA_MODZ_OFFLCLAL' 
+global.author = '@PODDA_MODZ_OFFLCLAL' 
+global.igfg = 'https://youtube.com/@PODDA_MODZ' 
+global.dygp = 'https://chat.whatsapp.com/JPubQQcQSt2A6RigyVG73h'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@PODDA_MODZ'
+global.fgpyp = 'https://youtube.com/@GAMING_PODDA_YT'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 
 
